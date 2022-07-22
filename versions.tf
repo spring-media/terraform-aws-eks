b85c6f5ccbf90fe5d15a80cdd56cbfd208d687b0
Bryant Biggs <bryantbiggs@gmail.com>
2022-07-22 08:29:00 -0400

    fix: Pin TLS provider version to 3.x versions only (#2174)

--- versions.tf
+++ versions.tf
@@ -8,7 +8,7 @@ terraform {
     }
     tls = {
       source  = "hashicorp/tls"
-      version = "< 4.0.0"
+      version = "~> 3.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
