4bf82d4760ab27ec76f1e131ad19b11bb88d7e50
Martin Weber <martin.weber@axelspringer.de>
2023-01-10 18:55:25 +0100

    release: version 19.5.1 from upstream

--- update-fork.sh
+++ update-fork.sh
@@ -2,11 +2,7 @@
 
 [ -z "$1" ] && echo "usage: update-fork.sh <upstream-version>" && exit
 
-git stash
-git pull --rebase git@github.com:terraform-aws-modules/terraform-aws-eks.git v$1 && \
-git rebase origin/spacelift
-git stash apply
-git stash drop
-yq e -i ".module_version = \"$1\"" .spacelift/config.yml && \
+git pull --autostash git@github.com:terraform-aws-modules/terraform-aws-eks.git v$1 && \
+yq e -i ".module_version = \"$1\"" .spacelift/config.yml #&& \
 git commit -am "release: version $1 from upstream" && \
 git push
